d7dcad27fad4304ac916051dc7e914c2ba7ea664
Danilo Toriy <randomlittlest@gmail.com>
2026-03-24 22:01:23 -0300

    feat: adicionando o repositório de Departamento

---
+++ src/main/java/com/example/danilo/repository/DepartamentoRepository.java
@@ -0,0 +1,7 @@
+package com.example.danilo.repository;
+
+import com.example.danilo.entity.Departamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+}
